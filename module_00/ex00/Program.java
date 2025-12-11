4202f8bac819b442062857eeec30c144f7749b6c
maaloum-yassine <maaloum.yassine@gmail.com>
2025-12-11 18:09:06 +0100

    module_00

---
+++ module_00/ex00/Program.java
@@ -0,0 +1,31 @@
+// public class Program
+// {
+//     public static void main(String[] args)
+//     {
+//         System.out.println("Byte size in bits: " + Byte.SIZE);
+//         System.out.println("Short size in bits: " + Short.SIZE);
+//         System.out.println("Long size in bits: " + Long.SIZE);
+//         System.out.println("Float size in bits: " + Float.SIZE);
+//         System.out.println("Double size in bits: " + Double.SIZE);        
+//         System.out.println("Integer size in bits: " + Integer.SIZE);
+//         System.out.println("Character size in bits: " + Character.SIZE);
+//         System.out.println("Boolean size in Java: JVM dependent, usually 1 byte");
+//     }
+// }
+
+
+
+
+public class  Program {
+    public static void main(String[] args) {
+        int number = 479598;
+        int sum = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            number = number / 10;
+            sum +=  digit;
+        }
+        System.out.println(sum);
+    }
+}
---
+++ module_00/ex01/Program.java
@@ -0,0 +1,14 @@
+public class Program
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Byte size in bits: " + Byte.SIZE);
+        System.out.println("Short size in bits: " + Short.SIZE);
+        System.out.println("Long size in bits: " + Long.SIZE);
+        System.out.println("Float size in bits: " + Float.SIZE);
+        System.out.println("Double size in bits: " + Double.SIZE);        
+        System.out.println("Integer size in bits: " + Integer.SIZE);
+        System.out.println("Character size in bits: " + Character.SIZE);
+        System.out.println("Boolean size in Java: JVM dependent, usually 1 byte");
+    }
+}
