d1150ec118bece793892f2ec3be7dca3e3d48d8d
maaloum-yassine <maaloum.yassine@gmail.com>
2026-08-22 17:11:18 +0100

    upadet

--- 00/ex01/Program.java
+++ 00/ex01/Program.java
@@ -2,53 +2,13 @@ import java.util.Scanner;
 
 public class Program
 {
-    public static int parseInteger(String strDigit)
+
+    public static int isPrimCount(int number)
     {
-        long res = 0;
-        int sign = 1;
-        int i = -1;
-        int lengthDigit = strDigit.length();
-
-        while (++i < lengthDigit) {
-            char c = strDigit.charAt(i);
-            if (!(c == ' ' || c == '\t' || c == '\n'))
-                break ;
-        }   
-        if ((i < lengthDigit) && (strDigit.charAt(i) == '-' || strDigit.charAt(i) == '+')) {
-            if (strDigit.charAt(i) == '-')
-                sign = -1;
-            i++;
-        }
-
-        while (i < lengthDigit) {
-            char c = strDigit.charAt(i); 
-            if (c >= '0' && c <= '9')
-            {
-                if ((res * sign > Integer.MAX_VALUE) || (res * sign < Integer.MIN_VALUE))
-                    return -2;
-                res = res * 10 + c - '0';
-            }
-            else 
-                break; 
-            i++;
-        }
-        if (i < lengthDigit)
-        {
-            char c = strDigit.charAt(i);
-            if (c != '\t' && c != ' ')
-                return  -1;
-        }
-        return ((int)res * sign);
-    }
-
-
-    static int isPrimCount(int number)
-    {
-
         int numberCheker = 2;
         int i = 1;
         int count = 1;
-
+    
         while (numberCheker * numberCheker <= number)
             numberCheker++;
         
@@ -64,19 +24,23 @@ public class Program
     public static void main (String args[])
     {
         Scanner sc = new Scanner(System.in);
-        String number_in = sc.next();   
-        int number = parseInteger(number_in);
 
-        if (number <= 1)
-            System.err.println("IllegalArgument");
-        else
+        if (sc.hasNextInt())
         {
-            int result = isPrimCount(number);
-            if (result > 0)
-                System.out.println("true " + result);
+            int number = sc.nextInt();
+            if (number <= 1)
+                System.out.print("IllegalArgument");
             else
-                System.out.println("false " + (-result));
+            {
+                int result = isPrimCount(number);
+                if (result > 0)
+                    System.out.println("true " + result);
+                else
+                    System.out.println("false " + (-result));
+            }
         }
+        else
+            System.out.print("IllegalArgument");
         sc.close();
     }
 }
