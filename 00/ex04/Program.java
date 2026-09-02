ca9b1022e357f71afd09e806ea63a412e3af7d29
maaloum-yassine <maaloum.yassine@gmail.com>
2026-09-02 01:25:05 +0100

    finish 04

--- 00/ex04/Program.java
+++ 00/ex04/Program.java
@@ -4,41 +4,44 @@ public class Program
 {
 
 
-    public static void dispalyFrequencyAnalysis(int[] arrangingAlpha, char[] repAlphabet)
+    public static void dispalyFrequencyAnalysis(int[] repAlphabet , char[] arrangingAlpha, int length)
     {
-        // for (int j = 10 ; j >= Res; j++)
-        // {
-        //     System.out.print();
-        // }
+        int[] heights = new int[length];
         
-        for (int  i = -1; i < 10 ; i++)
+        for (int  i = 0; i < length ; i++) {
+            heights[i] = (repAlphabet[i] * 10) / repAlphabet[0];
+        }
+        
+       for (int level = 10; level >= 0; level--)
         {
-            int Res =  (arrangingAlpha[i] * 10) / arrangingAlpha[i];
-            if (Res == )
+            for (int i = 0; i < length; i++) {
+                if (heights[i] == level )
+                    System.out.print(repAlphabet[i] + "\t");
+                else if (heights[i] >= level)
+                    System.out.print("# \t");
+                else 
+                    System.out.print("  \t");
+            }
+            System.out.println();
+        }
+        for (char c : arrangingAlpha) {
+                System.out.print(c + "  \t");
         }
     }
 
-    public static  int returnIndex(int repUniCode, int[] repAlphabet, char [] arrangingAlpha)
+    public static int returnIndex(int repUniCode, int[] repAlphabet, char[] arrangingAlpha)
     {
-        int index = 0;
+        int index;
         for (index = 0; index < repAlphabet.length; index++)
         {
-            if (repAlphabet[index] <= repUniCode)
+            if (repUniCode >= repAlphabet[index])
             {
-                for (int j = index; j < repAlphabet.length; j++)
+                for (int j = repAlphabet.length - 1; j > index; j--)
                 {
-                    if (repAlphabet[index] > repAlphabet[j])
-                    {
-                        int tmp = repAlphabet[j];
-                        repAlphabet[j] = repAlphabet[index];
-                        repAlphabet[index] =  tmp;
-
-                        char tmpChar = arrangingAlpha[j];
-                        arrangingAlpha[j] = arrangingAlpha[index];
-                        arrangingAlpha[index] =  tmpChar;
-                    }
+                    repAlphabet[j] = repAlphabet[j - 1];
+                    arrangingAlpha[j] = arrangingAlpha[j - 1];
                 }
-                break ;
+                break;
             }
         }
         return index;
@@ -46,32 +49,30 @@ public class Program
 
     public static void handlArrays (int [] unicodeChar)
     {
-            int correctIndex;
+        int correctIndex;
         char [] arrangingAlpha = new char[10];
         int [] repAlphabet = new int[10];
-
+        int length = 0;
 
         for (int i = 0; i < unicodeChar.length; i++)
         {
-                if (unicodeChar[i] > 0)
+            if (unicodeChar[i] > 0)
             {
                 correctIndex = returnIndex(unicodeChar[i], repAlphabet, arrangingAlpha);
-                repAlphabet[correctIndex] = unicodeChar[i];
-                arrangingAlpha[correctIndex] = (char)i;
+                if (correctIndex < repAlphabet.length)
+                {
+                    repAlphabet[correctIndex] = unicodeChar[i];
+                    arrangingAlpha[correctIndex] = (char)i;
+                }
             }
         }
-        dispalyFrequencyAnalysis(repAlphabet, arrangingAlpha);
 
-        // for (char c : arrangingAlpha) {
-            
-        //     System.out.println( "Array Char -------------<<<<>>>> " + c);
-        // }
-    
-        // for (int x : repAlphabet) {
-            
-        //     System.out.println( "repAlphabet  -------------<<<<>>>> " + x);
-        // }
-        
+        for(int i = 0 ; i < 10 ; i++)
+        {
+            if (repAlphabet[i] != 0)
+                length++;
+        }
+        dispalyFrequencyAnalysis(repAlphabet, arrangingAlpha, length);
     }
 
 
@@ -81,7 +82,9 @@ public class Program
         String input;
            
         Scanner sc = new Scanner(System.in);
-        input = "abbbcccccddddd11112222333555111      dddiiooo999999665555www";
+        input = sc.nextLine();
+        // input ="das;kdjalsdhd djaskldjalsjkdieq osdjas diqweiwke opauq9eqw1231290312738127jb,d k;dl *d/as*/dasdas4d65";
+        // input = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSSSSSSSSDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDWEWWKFKKDKKDSKAKLSLDKSKALLLLLLLLLLRTRTETWTWWWWWWWWWWOOOOOOO42";
         char [] inputCharArray = input.toCharArray();
         int [] unicodeChar =  new int [65536];
         
@@ -89,7 +92,7 @@ public class Program
         {
             for (int i = 0; i < inputCharArray.length; i++)
             {
-                if (unicodeChar[i] != 999)
+                // if (unicodeChar[i] != 999)
                     unicodeChar[inputCharArray[i]]++;    
             }
         }
